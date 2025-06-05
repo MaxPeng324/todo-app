b79c506836366985d026dfa785aa9aae0f96dbda
Mx.Peng <pq839556387@gmail.com>
2025-06-05 10:45:56 +0800

    fix: 修复tri方法不合法issue

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  * @Author: Mx.Peng
  * @Date: 2025-05-19 14:43:53
  * @LastEditors: Mx.Peng
- * @LastEditTime: 2025-06-05 10:20:27
+ * @LastEditTime: 2025-06-05 10:45:16
  * @Description: 
  */
  /**
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const todoList = document.getElementById("todo-list");
 
   function addTodo() {
-    const todoText = todoInput.value.tr();
+    const todoText = todoInput.value.trim();
     if (todoText === "") {
       alert("请输入待办事项");
       return;
